12ea027117e1509d0e1614016457b560d8f4a9a3
DanielGG50 <daniel@email.com>
2025-07-24 22:35:44 +0000

    si

--- formal/jg_fpv.tcl
+++ formal/jg_fpv.tcl
@@ -8,19 +8,20 @@ clear -all
 # Add Sub
 #analyze -sv ../rtl/normalize_rounder.v
 #analyze -sv ../rtl/mantissa_add_sub.v
-analyze -sv ../rtl/mantissa_shifter.v 
-#analyze -sv ../rtl/exponent_sub_upd.v
+#analyze -sv ../rtl/mantissa_shifter.v 
+analyze -sv ../rtl/exponent_sub.v
 #analyze -sv ../rtl/sign_logic.v
 #analyze -sv ../rtl/exception_block.v
 #analyze -sv ../rtl/add_sub_main.v
  
 #FV
-analyze -sv mantissa_shifter_fv.sv
+analyze -sv exponent_sub_fv.sv
+#analyze -sv mantissa_shifter_fv.sv
 #analyze -sv mantissa_add_sub_fv.sv
 #analyze -sv exception_block_fv.sv
 
 #REMEMBER TO CHAMGE TOP WHEN TESTING INDIVIDUAL MODULES
-elaborate -bbox_a 65535 -bbox_mul 65535 -top mantissa_shifter
+elaborate -bbox_a 65535 -bbox_mul 65535 -top exponent_sub
 
 clock clk
 
--- rtl/exponent_sub.v
+++ rtl/exponent_sub.v
@@ -1,32 +1,44 @@
-module  exponent_sub #(parameter EXP_WIDTH = 8)(    // Module that represents the EXP_WIDTH subtractor block
-    input [EXP_WIDTH-1: 0] exp_a, exp_b,
-    output reg [4:0] shift_spaces,
-    input sign_a, sign_b,
-    output reg [1:0] exp_disc,
-    output reg [EXP_WIDTH-1:0] exp_value, 
-    input operation_select  
+module exponent_sub #(parameter EXP_WIDTH = 8)(    // Module that represents the EXP_WIDTH subtractor block
+	input clk,	
+	input arst_n,
+	input [EXP_WIDTH-1: 0] exp_a, exp_b,
+	output reg [4:0] shift_spaces,
+	output reg [1:0] exp_disc,
+	output reg [EXP_WIDTH-1:0] exp_value 
 );    
 
-    wire a_greater, a_less, a_equal;
-    wire update_sign_b;
+	wire a_greater, a_less, a_equal;
 
-    assign a_greater = (exp_a > exp_b);        // Compare
-    assign a_less = (exp_a < exp_b);           // Imput 
-    assign a_equal = (exp_a == exp_b);         // Exponents
+	assign a_greater = (exp_a > exp_b);        // Compare
+	assign a_less = (exp_a < exp_b);           // Imput 
+	assign a_equal = (exp_a == exp_b);         // Exponents
 
-    assign update_sign_b = sign_b ^ operation_select;
+	always @(*) begin                        // Determins output case
+  	exp_disc_comb = a_greater ? 2'b10 : 
+		a_less ? 2'b00 : 2'b11; 
+	end
 
-    always @(*) begin                        // Determins output case
-        exp_disc = a_greater ? 2'b10 : 
-        a_less ? 2'b00 : 2'b11; 
-    end
-    
-    always @(*) begin
-        exp_value = (a_greater || a_equal) ? exp_a : exp_b;    // Outputs greater exponent
-    end         
+	always @(*) begin
+		exp_value_comb = (a_greater || a_equal) ? exp_a : exp_b;    // Outputs greater exponent
+	end         
 
-    always @(*) begin
-        shift_spaces = a_greater ? exp_a - exp_b : a_less ? exp_b - exp_a : 5'b00000;
-    end 
-    
+	always @(*) begin
+		shift_spaces_comb = a_greater ? exp_a - exp_b : a_less ? exp_b - exp_a : 5'b00000;
+	end 
+   
+	reg [4:0] shift_spaces_comb;
+	reg [1:0] exp_disc_comb;
+	reg [EXP_WIDTH-1:0] exp_value_comb;
+
+	always @(posedge clk, negedge arst_n)
+		if (!arst_n) begin
+			exp_value <= 0;
+			exp_disc <= 0;
+			shift_spaces <= 0;	
+ 		end else begin
+			exp_value <= exp_value_comb;
+			exp_disc <= exp_disc_comb;		
+			shift_spaces <= shift_spaces_comb;
+		end
+
 endmodule 
