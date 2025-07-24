b5e974d79aef28eb0f52e57f20e33246e918b047
DanielGG50 <daniel@email.com>
2025-07-24 01:49:32 +0000

    si

--- rtl/exponent_sub.v
+++ rtl/exponent_sub.v
@@ -3,8 +3,7 @@ module  exponent_sub #(parameter EXP_WIDTH = 8)(    // Module that represents th
     output reg [4:0] shift_spaces,
     input sign_a, sign_b,
     output reg [1:0] exp_disc,
-    output reg [EXP_WIDTH-1:0] exp_value,
-    output reg out_sign,  
+    output reg [EXP_WIDTH-1:0] exp_value, 
     input operation_select  
 );    
 
@@ -24,17 +23,10 @@ module  exponent_sub #(parameter EXP_WIDTH = 8)(    // Module that represents th
     
     always @(*) begin
         exp_value = (a_greater || a_equal) ? exp_a : exp_b;    // Outputs greater exponent
-    end   
-    
-    always @(*) begin
-	
-        out_sign = a_greater ? sign_a : update_sign_b;                // Outputs sign
-
-    end      
+    end         
 
     always @(*) begin
-        shift_spaces = a_greater ? exp_a - exp_b : 
-                    a_less ? exp_b - exp_a : 8'b00000000;
+        shift_spaces = a_greater ? exp_a - exp_b : a_less ? exp_b - exp_a : 8'b00000000;
     end 
     
 endmodule 
