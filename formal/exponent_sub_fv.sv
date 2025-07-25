5c41c33d2552bb14a6a335b334289be6b67cdea4
DanielGG50 <daniel.garcia@cinvestav.mx>
2025-07-24 22:51:51 -0600

    Update exponent_sub_fv.sv
    CHANGED ASSERTIONS TO UPDATED RTL

--- formal/exponent_sub_fv.sv
+++ formal/exponent_sub_fv.sv
@@ -20,15 +20,21 @@ module exponent_sub_fv #(parameter EXP_WIDTH = 8)(    // Module that represents 
 	localparam A_GREATER = 2'b10;
 	localparam A_LESS = 2'b00;
 	localparam A_EQUAL = 2'b11;
-
-	wire [4:0] x, y;
+		// Sifting max_value
+	localparam MAX_SHIFT = 2**5 -1;
+		// eliminated [4:0]
+	wire [EXP_WIDTH-1:0] x, y;
 	assign x = exp_a - exp_b;
 	assign y = exp_b - exp_a;
-
-	`AST(exp_value, a_greater, (exp_a > exp_b) |=>, (exp_disc == A_GREATER) && (exp_value == $past(exp_a)) && (shift_spaces == $past(x)) )
-	`AST(exp_value, b_greater, (exp_b > exp_a) |=>, (exp_disc == A_LESS) && (exp_value == $past(exp_b)) && (shift_spaces == $past(y)) )
+		
+	//`AST(exp_value, a_greater, (exp_a > exp_b) |=>, (exp_disc == A_GREATER) && (exp_value == $past(exp_a)) && (shift_spaces == $past(x)) )
+	//`AST(exp_value, b_greater, (exp_b > exp_a) |=>, (exp_disc == A_LESS) && (exp_value == $past(exp_b)) && (shift_spaces == $past(y)) )
 	`AST(exp_value, a_b_equal, (exp_b == exp_a) |=>, (exp_disc == A_EQUAL) && (exp_value == $past(exp_a)) && (shift_spaces == 0) )
-
+	`AST(exp_value, a_greater, (exp_a > exp_b) && (x < MAX_SHIFT) |=>, (exp_disc == A_GREATER) && (exp_value == $past(exp_a)) && (shift_spaces == $past(x)) )
+	`AST(exp_value, b_greater, (exp_b > exp_a) && (y < MAX_SHIFT) |=>, (exp_disc == A_LESS) && (exp_value == $past(exp_b)) && (shift_spaces == $past(y)) )
+	`AST(exp_value, a_greater_max_shift, (exp_a > exp_b) && (x >= MAX_SHIFT) |=>, (exp_disc == A_GREATER) && (exp_value == $past(exp_a)) && (shift_spaces == MAX_SHIFT) )
+		`AST(exp_value, B_greater_max_shift, (exp_b > exp_a) && (y >= MAX_SHIFT) |=>, (exp_disc == A_LESS) && (exp_value == $past(exp_b)) && (shift_spaces == MAX_SHIFT) )
+		
 endmodule
 
 bind exponent_sub exponent_sub_fv exponent_sub_fv_i (.*);
